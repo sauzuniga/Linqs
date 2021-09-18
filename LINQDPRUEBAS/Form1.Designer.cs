91845b54a9be655f67a3ac358180216b9c0cfc2f
sauzuniga <88364351+sauzuniga@users.noreply.github.com>
2021-09-17 22:50:28 -0600

    Se agrega todas las pruebas de lo que es linq

---
+++ LINQDPRUEBAS/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace LINQDPRUEBAS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtanswer = new System.Windows.Forms.TextBox();
+            this.lbusuarios = new System.Windows.Forms.ListBox();
+            this.btnbuscar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(205, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuarios activos del sitio de streaming";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 190);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(418, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Digite enable si quiere ver los usuarios activos o digite disable si quiere ver l" +
+    "os inactivos";
+            // 
+            // txtanswer
+            // 
+            this.txtanswer.Location = new System.Drawing.Point(526, 174);
+            this.txtanswer.Name = "txtanswer";
+            this.txtanswer.Size = new System.Drawing.Size(142, 20);
+            this.txtanswer.TabIndex = 2;
+            // 
+            // lbusuarios
+            // 
+            this.lbusuarios.FormattingEnabled = true;
+            this.lbusuarios.Location = new System.Drawing.Point(229, 314);
+            this.lbusuarios.Name = "lbusuarios";
+            this.lbusuarios.Size = new System.Drawing.Size(369, 134);
+            this.lbusuarios.TabIndex = 3;
+            // 
+            // btnbuscar
+            // 
+            this.btnbuscar.Location = new System.Drawing.Point(61, 257);
+            this.btnbuscar.Name = "btnbuscar";
+            this.btnbuscar.Size = new System.Drawing.Size(147, 42);
+            this.btnbuscar.TabIndex = 4;
+            this.btnbuscar.Text = "Buscar";
+            this.btnbuscar.UseVisualStyleBackColor = true;
+            this.btnbuscar.Click += new System.EventHandler(this.btnbuscar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnbuscar);
+            this.Controls.Add(this.lbusuarios);
+            this.Controls.Add(this.txtanswer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtanswer;
+        private System.Windows.Forms.ListBox lbusuarios;
+        private System.Windows.Forms.Button btnbuscar;
+    }
+}
+
---
+++ LINQDPRUEBAS/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LINQDPRUEBAS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnbuscar_Click(object sender, EventArgs e)
+        {
+            List<string> usuarios = new List<string>() { "Maria Villanueva enable" ,"Diego Rivera enable" ,"Diego Arismas disable","Naun Alvarez disable" ,"Cesar Galeas enable"  };
+            string answers = txtanswer.Text;
+            if (answers=="enable")
+            { 
+            IEnumerable<string> users = from i in usuarios
+                                        where i.EndsWith(answers)
+                                        select i;
+                lbusuarios.Items.Add("Los usuarios activos son:");
+                foreach (string i in users)
+            lbusuarios.Items.Add( i );
+            }
+            else
+            {
+                IEnumerable<string> users = from i in usuarios
+                                            where i.EndsWith(answers)
+                                            select i;
+                lbusuarios.Items.Add("Los usuarios inactivo son: " );
+                foreach (string i in users)
+                    lbusuarios.Items.Add("Los usuarios inactivos son " + i);
+            }
+        }
+    }
+}
